04ed1bbe43bd0b71ac7d9439a315ebbbec313a9f
Christine Nguyen <ctine.ngyn@gmail.com>
2021-02-25 03:26:09 -0800

    Adding 5 questions, choices and answers

---
+++ script.js
@@ -0,0 +1,27 @@
+// Array of quiz questions, choices and correct answers
+var questions = [{
+    question: 'Commonly used data types DO NOT include:',
+    choices: ['Strings()', 'Booleans()', 'Alerts()', 'Numbers()'],
+    answer: 'Alerts()'
+},
+{
+    question: 'The condition in an if/else statement is enclosed within _____.',
+    choices: ['Quotes()', 'Curly Brackets()', 'Parentheses()', 'Square Brackets()'],
+    answer: 'Parentheses()'
+},
+{
+    question: 'Arrays in JavaScript can be used to store _____.',
+    choices: ['Numbers and Strings()', 'Other Arrays()', 'Booleans()', 'All of the Above()'],
+    answer: 'All of the Above()'
+},
+{
+    question: 'String values must be enclosed within _____ when being assigned to variables.',
+    choices: ['Commas()', 'Curly Brackets()', 'Quotes()', 'Parentheses()'],
+    answer: 'Quotes()'
+},
+{
+    question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+    choices: ['JavaScript()', 'Terminal/Bash()', 'For Loops()', 'Console.log()'],
+    answer: 'Console.log()'
+    }
+]
